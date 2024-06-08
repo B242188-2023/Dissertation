cc04c272d1008fa9612a90a0414237e815ae42bb
B242188-2023 <none>
2024-06-08 20:12:15 +0100

    samtools & bcftools

--- dissertation/40-1000082264/alignment/sam-bam.sh
+++ dissertation/40-1000082264/alignment/sam-bam.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 samples=("LD1" "LD2" "LL1" "LL2" "SD1" "SD2" "SL1" "SL2")
+# samples=("LD1")
 for sample in "${samples[@]}"
 do
     echo "Processing $sample..."
-    # format conversion sam->bam
-    samtools view -b ${sample}_aligned.sam -o ${sample}_aligned.bam
-    # sort
-    samtools sort -o ${sample}_sorted.bam ${sample}_aligned.bam
+
+    # extracting unmatched reads 
+    samtools view -@ 160 -b -f 4 ${sample}_cds_sorted.bam > ${sample}_cds_unmapped_sorted.bam
+    # bam -> fastq
+    samtools fastq ${sample}_cds_unmapped_sorted.bam > ${sample}_cds_unmapped.fastq
+    # de novo assembly
+    spades.py -s ${sample}_cds_unmapped.fastq -o spades_output_${sample}
+    # matching
+    bowtie2 -x ../fastq/Index-cds -f spades_output_${sample}/contigs.fasta\
+     | samtools view -bS - | samtools sort -o ${sample}_cds_denovo_sorted.bam
     
+    
+    # sort
+    # samtools sort -@ 160 ${sample}_cds_unmapped.bam -o ${sample}_cds_unmapped_sorted.bam
+    # statistical analyze
+    #samtools flagstat ${sample}_cds_sorted.bam > ${sample}_cds_sorted.stats
+    
+    # index
+    # samtools index -@ 160 ${sample}_index ${sample}_cds_unmapped_sorted.bam
+
+    # identify variations
+    bcftools mpileup --threads 160 -Ou -f ../fastq/AeUmbellulata_TA1851_v1-cds.fasta\
+     ${sample}_cds_denovo_sorted.bam | bcftools call -mv -o ${sample}_variants.vcf
+
+    # filtering low quality variants
+    bcftools view ${sample}_variants.vcf | bcftools filter --exclude 'QUAL < 10' \
+    > ${sample}_filtered_variants.vcf
+
+    bcftools view -v snps -m2 -M2 --min-ac 1:minor ${sample}_filtered_variants.vcf > ${sample}_snp.vcf
     echo "Completed processing $sample."
 done
