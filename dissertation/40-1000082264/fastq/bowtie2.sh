736cf02a024997cf24f68333a2fcb898c80b9db7
B242188-2023 <none>
2024-06-08 20:10:34 +0100

    reduce process documentation

--- dissertation/40-1000082264/fastq/bowtie2.sh
+++ dissertation/40-1000082264/fastq/bowtie2.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-bowtie2-build AeUmbellulata_TA1851_v1.fasta Index
+#bowtie2-build AeUmbellulata_TA1851_v1-cds.fasta Index-cds
+#bowtie2-build AeUmbellulata_TA1851_v1.fasta Index
 
 samples=("LD1" "LD2" "LL1" "LL2" "SD1" "SD2" "SL1" "SL2")
+#samples=("LD1")
+
 for sample in "${samples[@]}"
 do
     echo "Processing $sample..."
-
-    bowtie2 -x Index -1 ${sample}_R1_dedup.fastq -2 ${sample}_R2_dedup.fastq -S ../alignment/${sample}_aligned.sam -p 32
+    bowtie2 -x Index-cds -1 ${sample}_R1_processed.fastq -2 ${sample}_R2_processed.fastq \
+    -p 32 --very-sensitive-local | \
+    samtools sort -@ 160 -o ../alignment/${sample}_cds_sorted.bam
     echo "Completed aligning $sample."
 done
