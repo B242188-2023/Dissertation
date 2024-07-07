62bb1b3b44301defb13a871431692508e800f842
B242188-2023 <none>
2024-07-07 19:52:29 +0100

    snpEff

--- dissertation/40-1000082264/alignment/feature annotation.sh
+++ dissertation/40-1000082264/alignment/feature annotation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-snpEff build -gff3 -v AeUmbellulata -c snpEff.config > output.log 2>&1
-python3 id-transform.py 
+#snpEff build -gff3 -v AeUmbellulata -c snpEff.config > output.log 2>&1
+#python3 id-transform.py 
 
 samples=("LD1" "LD2" "LL1" "LL2" "SD1" "SD2" "SL1" "SL2")
 #samples=("LD1")
 for sample in "${samples[@]}"
 do
     echo "Processing $sample..."
-    bcftools annotate --rename-chrs rename.txt ${sample}_variants.vcf\
-     | snpEff ann AeUmbellulata - -c snpEff.config > \
+    #bcftools annotate --rename-chrs rename.txt ${sample}_snp.vcf -o ${sample}_snp_renamed.vcf
+
+    snpEff ann -htmlStats ../annotation/${sample}_snpEff_report.html \
+     AeUmbellulata ${sample}_output.vcf -c snpEff.config > \
      ../annotation/${sample}_annotated_variants.vcf
     echo "Completed processing $sample."
 done
