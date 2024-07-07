ec177616e178df3978b065c88c625d13ba206728
B242188-2023 <none>
2024-07-07 19:56:02 +0100

    featureCounts & SNP vcf annotation

--- dissertation/40-1000082264/alignment/sam-bam.sh
+++ dissertation/40-1000082264/alignment/sam-bam.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 samples=("LD1" "LD2" "LL1" "LL2" "SD1" "SD2" "SL1" "SL2")
-# samples=("LD1")
+#samples=("LD1")
 for sample in "${samples[@]}"
 do
     echo "Processing $sample..."
 
     # extracting unmatched reads 
-    samtools view -@ 160 -b -f 4 ${sample}_cds_sorted.bam > ${sample}_cds_unmapped_sorted.bam
+    #samtools view -@ 160 -b ${sample}_cds_sorted.bam > ${sample}_cds_mapped_sorted.bam
     # bam -> fastq
-    samtools fastq ${sample}_cds_unmapped_sorted.bam > ${sample}_cds_unmapped.fastq
+    #samtools fastq ${sample}_cds_mapped_sorted.bam > ${sample}_cds_mapped.fastq
     # de novo assembly
-    spades.py -s ${sample}_cds_unmapped.fastq -o spades_output_${sample}
+    #spades.py -s ${sample}_cds_unmapped.fastq -o spades_output_${sample}
     # matching
-    bowtie2 -x ../fastq/Index-cds -f spades_output_${sample}/contigs.fasta\
-     | samtools view -bS - | samtools sort -o ${sample}_cds_denovo_sorted.bam
+    #bowtie2 -x ../fastq/Index-cds -f spades_output_${sample}/contigs.fasta\
+    # | samtools view -bS - | samtools sort -o ${sample}_cds_denovo_sorted.bam
     
     
     # sort
@@ -22,16 +22,19 @@ do
     #samtools flagstat ${sample}_cds_sorted.bam > ${sample}_cds_sorted.stats
     
     # index
-    # samtools index -@ 160 ${sample}_index ${sample}_cds_unmapped_sorted.bam
+    # samtools index -@ 160 ${sample}_cds_sorted.bam
+
+    featureCounts -a AeUmbellulata_TA1851_v1.gtf -o ${sample}_counts.txt \
+    ${sample}_genome_sorted.bam > ${sample}_featureCounts.log 2>&1
 
     # identify variations
     bcftools mpileup --threads 160 -Ou -f ../fastq/AeUmbellulata_TA1851_v1-cds.fasta\
-     ${sample}_cds_denovo_sorted.bam | bcftools call -mv -o ${sample}_variants.vcf
+     ${sample}_cds_sorted.bam | bcftools call -mv -Oz -o ${sample}_variants.vcf.gz
 
     # filtering low quality variants
-    bcftools view ${sample}_variants.vcf | bcftools filter --exclude 'QUAL < 10' \
-    > ${sample}_filtered_variants.vcf
+    bcftools view ${sample}_variants.vcf.gz | bcftools filter --exclude 'QUAL < 20' \
+    | bgzip -c > ${sample}_filtered_variants.vcf.gz
 
-    bcftools view -v snps -m2 -M2 --min-ac 1:minor ${sample}_filtered_variants.vcf > ${sample}_snp.vcf
+    bcftools view -v snps -m2 -M2 --min-ac 1:minor ${sample}_filtered_variants.vcf.gz > ${sample}_snp.vcf
     echo "Completed processing $sample."
 done
